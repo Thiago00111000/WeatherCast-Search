a32c03d739be1a33f77526db6a92e0d27494b23b
Thiago00111000 <127748415+Thiago00111000@users.noreply.github.com>
2023-11-25 12:32:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-$ curl "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+//const APIweatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e033f0bcf7cac98d13267ff1a51dab46&lang=pt_br`;
+//const APIkey = " e033f0bcf7cac98d13267ff1a51dab46";
 
-{
-  "current": {
-    "time": "2022-01-01T15:00"
-    "temperature_2m": 2.4,
-    "wind_speed_10m": 11.9,
-  },
-  "hourly": {
-    "time": ["2022-07-01T00:00","2022-07-01T01:00", ...]
-    "wind_speed_10m": [3.16,3.02,3.3,3.14,3.2,2.95, ...],
-    "temperature_2m": [13.7,13.3,12.8,12.3,11.8, ...],
-    "relative_humidity_2m": [82,83,86,85,88,88,84,76, ...],
-  }
+const cityInput = document.querySelector("#city-input"); 
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherElement = document.querySelector("#weather-icon")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
+const pressureElement = document.querySelector("#pressure span")
+
+//f(x) 
+const getWeatherData = async(city) => {
+  const APIweatherURL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e033f0bcf7cac98d13267ff1a51dab46&lang=pt_br`;
+        const res = await fetch(APIweatherURL);
+        const data = await res.json();
+        return data;
 }
+
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  //weatherElement.setAttribute = ("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed} km/h`;
+  pressureElement.innerText = `${data.main.pressure} hPa`;
+};
+
+//evento
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+    const city =  cityInput.value;
+
+  showWeatherData(city);
+
+})
